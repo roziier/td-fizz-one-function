4c09fe6f8f1329f5934db9cf9f0fce077a5986d1
roziier <rikirosa93@gmail.com>
2021-08-19 11:23:42 +0200

    webpack css - sass installed

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
-const path = require("path");
+var path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "./dist"),
+  },
+  mode: "none",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
   },
 };
